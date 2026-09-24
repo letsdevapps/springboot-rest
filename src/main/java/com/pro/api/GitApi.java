8221f5812ccfdb8270a775e44c32d614fa0b1a75
letsdevapps <letsdevapps@gmail.com>
2026-09-24 15:47:26 -0300

    Update GitApi with git commit info to be used on Render

--- src/main/java/com/pro/api/GitApi.java
+++ src/main/java/com/pro/api/GitApi.java
@@ -2,6 +2,8 @@ package com.pro.api;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.info.GitProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,18 +14,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class GitApi {
 
 	private final GitProperties gitProperties;
+	private final String renderCommit;
 
-	public GitApi(GitProperties gitProperties) {
+	public GitApi(@Autowired(required = false) GitProperties gitProperties,
+			@Value("${RUNTIME_COMMIT_SHA:unknown}") String renderCommit) {
+
 		this.gitProperties = gitProperties;
-	}
-
-	@GetMapping
-	public GitProperties index() {
-		return gitProperties;
+		this.renderCommit = renderCommit;
 	}
 
 	@GetMapping("/version")
 	public Map<String, String> version() {
+		if (gitProperties == null) {
+			return Map.of("commit", renderCommit);
+		}
+
 		return Map.of("commit", gitProperties.getShortCommitId(), "branch", gitProperties.getBranch());
 	}
 }
